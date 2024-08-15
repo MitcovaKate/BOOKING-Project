f72266dd3a9236929895ee4d49208195998df43f
MitcovaKate <katya.mitkova@bk.ru>
2024-08-15 15:59:24 +0300

    composer

--- website/index.php
+++ website/index.php
@@ -1,2 +1,8 @@
 <? 
-phpinfo();
+require_once './vendor/autoload.php';
+
+use Student\Booking\Client;
+use Student\Booking\Tour;
+
+$client = new Client();
+$tour= new Tour();
---
+++ website/src/Tour.php
@@ -0,0 +1,3 @@
+<?
+namespace Student\Booking;
+class Tour{}
