190856dfbc92196b64cb5a2478647c6c7c066442
MitcovaKate <katya.mitkova@bk.ru>
2024-08-18 21:03:27 +0300

    template twig

--- website/index.php
+++ website/index.php
@@ -1,3 +1,17 @@
 <? 
 require_once './vendor/autoload.php';
-
+#twig config
+$loader = new \Twig\Loader\FilesystemLoader('./templates');
+$twig = new \Twig\Environment($loader, [
+// 'cache' => '/path/to/compilation_cache',
+]);
+$title='Booking Agency';
+$tours=[
+    ['title'=>'Expensive Tour','price'=>'100Euro'],
+    ['title'=>'Cheap Tour','price'=>'10Euro'],
+    ['title'=>'Optimal Tour','price'=>'50Euro']
+];
+    
+$template = $twig->load('home.twig.html');
+echo $template->render(['title' => $title,
+                        'tours'=>$tours]);
